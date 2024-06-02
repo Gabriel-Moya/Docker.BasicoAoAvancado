f732925b1c91afb3e2ebbe65a1e6bb7e4d17dc94
Gabriel Moya <gabrielmoya123@gmail.com>
2024-06-01 23:17:24 -0400

    feat: injetando variavel ambiente

--- PrimeiroDockerfile-NodeJs/Build/app.js
+++ PrimeiroDockerfile-NodeJs/Build/app.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 const hostname = '0.0.0.0';
-const port = 3000;
+const port = process.env.PORT;
+const version = process.env.VERSION;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -10,7 +11,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/ and version ${version}`);
 });
 
 process.on('SIGINT', function() {
